ba8d507cdd393211239c497143bb0995cdf750dd
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-02-14 21:43:07 +0200

    flake.nix init

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = { };
+  outputs = { self, ... } @ inputs: {
+    overlays.default = final: prev: {
+      st =
+        prev.st.overrideAttrs
+          (_oldAttrs: {
+            src = ./.;
+            conf = ./config.h;
+          });
+    };
+  };
+}
